bdbf77162991988a205c1727c7105cd945a20aaa
PuntusEugene <kapral.20.it@gmail.com>
2018-01-30 00:39:27 +0300

    initial commit

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae89ee7c-8b9b-4a9a-adb7-4975a6d598ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Ninject.Web.Common.WebHost;
+using SportsStore.WebUI.Infrastructure;
+
+namespace SportsStore.WebUI
+{
+    public class MvcApplication : NinjectHttpApplication
+    {
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private void RegisterServices(IKernel kernel)
+        {
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
+
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
